8bcfa324aa93ebd794c4f04448ffd85eb99fb2ea
Zuhaib <theshahzuhaib96@gmail.com>
2022-02-03 18:09:07 +0530

    Add: Seperation of concers

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
 import bubbleSort from './bubbleSort.js';
 
-bubbleSort();
+function generateRandomColor() {
+   var letters = '0123456789ABCDEF';
+   var color = '#';
+   for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+   }
+   return color;
+}
+
+const arr = [];
+
+// Generate random numbers and push them into the array
+for (let i = 0; i < 100; i++) {
+   randNumber = Math.floor(Math.random() * 56);
+   randColor = generateRandomColor();
+   arr.push({
+      y: randNumber,
+      color: randColor,
+      label: randNumber,
+   });
+}
+
+// Select chart in the DOM and set different properties
+const chart = new CanvasJS.Chart('chartContainer', {
+   animationEnabled: true,
+   theme: 'light2',
+   title: {
+      text: 'Sorting Visualizer',
+   },
+   data: [
+      {
+         type: 'column',
+         dataPoints: arr,
+      },
+   ],
+});
+
+chart.render();
+
+bubbleSort(chart);
--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,41 +1,4 @@
-function generateRandomColor() {
-   var letters = '0123456789ABCDEF';
-   var color = '#';
-   for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-   }
-   return color;
-}
-
-const arr = [];
-
-// Generate random numbers and push them into the array
-for (let i = 0; i < 100; i++) {
-   randNumber = Math.floor(Math.random() * 56);
-   randColor = generateRandomColor();
-   arr.push({
-      y: randNumber,
-      color: randColor,
-      label: randNumber,
-   });
-}
-
-// Select chart in the DOM and set different properties
-const chart = new CanvasJS.Chart('chartContainer', {
-   animationEnabled: true,
-   theme: 'light2',
-   title: {
-      text: 'Sorting Visualizer',
-   },
-   data: [
-      {
-         type: 'column',
-         dataPoints: arr,
-      },
-   ],
-});
-
-let bubbleSort = () => {
+function bubbleSort(chart) {
    let data = chart.options.data[0].dataPoints;
    let length = data.length;
    let i = 0;
@@ -60,7 +23,6 @@ let bubbleSort = () => {
          }, 0.5);
       }
    })();
-};
+}
 
-chart.render();
 export default bubbleSort;
