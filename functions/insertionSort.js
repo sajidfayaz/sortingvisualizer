e12dacb4a5825e51ab017f527fca4d879a034605
Sajid <sajidfayaz.work@gmail.com>
2022-02-08 19:17:55 +0530

    fix bugs

--- functions/insertionSort.js
+++ functions/insertionSort.js
@@ -1,16 +1,17 @@
 let insertionSort = async (arr, states) => {
-   let j;
+   let j; let key;
    for (let i = 1; i < arr.length; i++) {
-      j = i;
-
-      while (j >= 0 && arr[j] < arr[j - 1]) {
-         await swap(arr, j, j - 1);
-         // arr[j+1] = arr[j];
-         states[j] = 0;
-         states[j - 1] = 0;
+      key = arr[i];
+      j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+         states[j] = 2;
+         arr[j+1] = arr[j];
+         // states[j+1] = 0;
          j -= 1;
       }
-
+      await sleep(100);
+      arr[j+1] = key;
+      states[j+1] = 0;
       if (i == arr.length - 1) {
          for (let i = 0; i < arr.length; i++) states[i] = 1;
       }
